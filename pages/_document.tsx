89244a229e7b2cfedd5c765ba60537f5a149b7ca
OrkWard <orkward_o@outlook.com>
2024-07-12 23:59:38 +0800

    feat: init

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <title>验证码是多少！？</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
