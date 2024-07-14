67219fb693b4727bf213644e99f437770ea947ae
OrkWard <orkward_o@outlook.com>
2024-07-14 12:18:13 +0800

    feat: use backend to get code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,51 @@
 import { Inter } from "next/font/google";
-import fs from 'fs/promises'
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({code, mtime}: InferGetServerSidePropsType<typeof getServerSideProps> ){
-  const typography = 'font-sans  antialiased leading-relaxed text-inherit text-lg'
+export default function Home({
+  code,
+  mtime,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const typography =
+    "font-sans  antialiased leading-relaxed text-inherit text-lg";
 
   return (
     <>
-    <Head>
+      <Head>
         <title>验证码是多少！？</title>
-    </Head>
-    <main
-      className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
-    >
-      <div className="relative flex flex-col place-items-center before:-z-10 before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <p className={`${typography}`}>上次获取时间：{mtime}</p>
-        <p className={`${typography}`}>验证码：{code}</p>
-      </div>
-    </main>
+      </Head>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
+      >
+        <div className="relative flex flex-col place-items-center before:-z-10 before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+          <p className={`${typography}`}>上次获取时间：{mtime}</p>
+          <p className={`${typography}`}>验证码：{code}</p>
+        </div>
+      </main>
     </>
   );
 }
 
-export const getServerSideProps = (async () => {
-  const code = await fs.readFile('/mount/code', 'utf8');
-  const {mtime} = await fs.stat('/mount/code')
-  return {props: {code, mtime: mtime.toLocaleString('zn-CN')}};
-}) satisfies GetServerSideProps<{code: string, mtime: string}>;
+interface Code {
+  code: string;
+  mtime: string;
+}
 
+async function getCode() {
+  if (!process.env.BACKEND_URL) {
+    throw new Error("没有找到 bot 服务");
+  }
+
+  const res = await fetch(`${process.env.BACKEND_URL}`);
+  // const j = await res.json()
+  // console.log(j);
+  // return j
+
+  return await (res.json() as Promise<Code>);
+}
+
+export const getServerSideProps = (async () => {
+  return { props: await getCode() };
+}) satisfies GetServerSideProps<Code>;
